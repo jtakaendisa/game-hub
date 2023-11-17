cb1ca284c47853304cbd9d6dd156fe35e5395228
James Takaendisa <james.takaendisa01@gmail.com>
2023-11-17 12:48:26 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
+  const gridTemplateBase = `
+    "nav"
+    "main"
+  `;
+
+  const gridTemplateLarge = `
+  "nav nav"
+  "aside main"
+`;
+
   return (
     <div>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: gridTemplateBase,
+          lg: gridTemplateLarge,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </div>
   );
 }
