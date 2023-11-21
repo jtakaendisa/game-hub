7c72bf3e3e5533a296b40ace509342e3477fea97
James Takaendisa <james.takaendisa01@gmail.com>
2023-11-21 12:18:25 +0200

    Refactor: remove duplicate interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,12 +3,7 @@ import { GameQuery } from '../App';
 import apiClient from '../services/api-client';
 import { FetchResponse } from '../services/api-client';
 import { Genre } from './useGenres';
-
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { Platform } from './usePlatforms';
 
 export interface Game {
   id: number;
