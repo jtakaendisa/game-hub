a854458e6898aeaf2e71b72fc654eadac3fa8b35
James Takaendisa <james.takaendisa01@gmail.com>
2023-11-27 21:17:18 +0200

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,5 +1,6 @@
 import { Genre } from './Genre';
 import { Platform } from './Platform';
+import { Publisher } from './Publisher';
 
 export interface Game {
   id: number;
@@ -8,6 +9,7 @@ export interface Game {
   description_raw: string;
   slug: string;
   parent_platforms: { platform: Platform }[];
+  publishers: Publisher[];
   metacritic: number;
   genres: Genre[];
   rating_top: number;
